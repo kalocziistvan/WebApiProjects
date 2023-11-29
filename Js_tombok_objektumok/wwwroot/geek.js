07c420a9399ca65c0fc5f81832e15617c00aace9
kalocziistvan <kalocziisti@gmail.com>
2023-11-29 15:53:21 +0100

    Jss gyakorlás

---
+++ Js_tombok_objektumok/wwwroot/geek.js
@@ -0,0 +1,19 @@
+﻿var viccek
+window.onload = letöltés()
+function letöltés() {
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    for (var i = 0; i < d.length; i++) {
+        console.log(d[i].question)
+        let elem = document.createElement("div")
+        elem.innerHTML = d[i].question
+        ide.appendChild(elem)
+    }
+}
---
+++ WebAPIBase/wwwroot/scripts/jsgyak.js
@@ -0,0 +1,29 @@
+﻿window.onload = function () {
+    for (var sor = 0; sor < 10; sor++) {
+        //új div létrehozása az új sornak
+        var ujdiv = document.createElement('div');
+        //új div osztálylistájához add hozzá a "sor"-t
+        ujdiv.classList.add('sor');
+        //új div-et add hozzá a "pascal" gyermekeihez
+        var pascaldiv = document.getElementById('pascal');
+        pascaldiv.appendChild(ujdiv);
+        for (var oszlop = 0; oszlop <= sor; oszlop++) {
+            //új div létrehozása az új elemnek
+            var newdiv = document.createElement('div');
+            //új elem div osztálylistájához add hozzá az "elem"-et
+            newdiv.classList.add('elem');
+            //teszteléshet .innerHTML = `${sor}:${oszlop}`
+            newdiv.innerText = faktoriális(sor) / (faktoriális(oszlop) * faktoriális(sor - oszlop));
+            //legyen az innerHTML a megfelelő szám
+            //új elem div-et vedd fel a sor elemei közé
+            ujdiv.appendChild(newdiv);
+        }
+    }
+};
+var faktoriális = function (n) {
+    let er = 1;
+    for (let i = 2; i <= n; i++) {
+        er = er * i;
+    }
+    return er;
+}
